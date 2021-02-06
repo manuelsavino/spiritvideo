e1f81ab7334837384a728b67fc873f5125727f21
manuelsavino <manuelsavino@gmail.com>
2021-02-06 13:19:33 -0500

    add pdf

--- pages/video.js
+++ pages/video.js
@@ -1,6 +1,16 @@
+import { useState } from 'react';
 import Head from 'next/head';
+import { Document, pdfjs, Page } from 'react-pdf';
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
+import { SizeMe } from 'react-sizeme';
 
 export default function Home() {
+  const [numPages, setNumPages] = useState(null);
+  const [pageNumber, setPageNumber] = useState(2);
+  function onDocumentLoadSuccess({ numPages }) {
+    setNumPages(numPages);
+  }
+
   return (
     <div>
       <Head>
@@ -8,14 +18,26 @@ export default function Home() {
         <link rel='icon' href='/favicon.ico' />
       </Head>
       <div className='min-h-screen bg-gray-50 flex flex-col items-center justify-center py-12 sm:px-6 lg:px-8'>
-        <iframe
-          width={560}
-          height={315}
-          src='https://www.youtube.com/embed/DbyFM6xtN0E'
-          frameBorder={0}
-          allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
-          allowFullScreen
-        />
+        <Document file='/spiritFA.pdf' onLoadSuccess={onDocumentLoadSuccess}>
+          <Page pageNumber={pageNumber} />
+        </Document>
+        <p>
+          Page {pageNumber} of {numPages}
+        </p>
+        <div className='flex'>
+          <button
+            onClick={() => setPageNumber(pageNumber === 1 ? 1 : pageNumber - 1)}
+          >
+            Previous
+          </button>
+          <button
+            onClick={() =>
+              setPageNumber(pageNumber === 37 ? 37 : pageNumber + 1)
+            }
+          >
+            Next
+          </button>
+        </div>
       </div>
     </div>
   );
